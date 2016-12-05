7322cc2a5a0ca38c1f4bace31383f76bde847d23
RagnarArdal <RagnarArdal@gmail.com>
2016-12-05 18:03:32 +0000

    Using encryption, looks good! Yay

--- chat/tests/crypto/test_RSA.py
+++ chat/tests/crypto/test_RSA.py
@@ -3,15 +3,15 @@ from chat.misc.encoding_tools import ENCODING
 
 
 def test_rsa_encrypt_decrypt_1():
-    private, public = RSA.generate_key_pair()
+    public, private = RSA.generate_key_pair()
     cipher = RSA()
     message = bytes("Hello, world! I'm @ the c3||7re?", ENCODING)
     assert cipher.decrypt(cipher.encrypt(message, public), private) == message
 
 def test_rsa_encrypt_decrypt_2():
-    private, public = RSA.generate_key_pair()
+    public, private = RSA.generate_key_pair()
     message = bytes("A hopefully non sixteen character message", ENCODING)
     cipher = RSA()
-    cipher.set_private_key(private)
     cipher.set_public_key(public)
+    cipher.set_private_key(private)
     assert cipher.decrypt(cipher.encrypt(message)) == message
--- chat/tests/test_peer.py
+++ chat/tests/test_peer.py
@@ -5,16 +5,17 @@ from chat.crypto import RSA
 message = "The default message"
 
 
-def test_send_receive(self):
-    private, public = RSA.generate_key_pair()
+def test_send_receive():
+    public, private = RSA.generate_key_pair()
 
-    sender_address = ("127.0.0.1", 9999)
-    receiver_address = ("127.0.0.1", 8000)
+    receiver_port = 8000
+    receiver_address = ("127.0.0.1", receiver_port)
 
-    with Peer(sender_address) as sender:
-        sender.set_public_key(public)
-        sender.send_to(receiver_address, message)
+    receiver = Peer(port=receiver_port)
+    receiver.set_private_key(private)
 
-    with Peer(receiver_address) as receiver:
-        receiver.set_private_key(private)
-        assert receiver.receive() == message
+    sender = Peer()
+    sender.set_public_key(public)
+    sender.send(receiver_address, message)
+
+    assert receiver.receive() == message
