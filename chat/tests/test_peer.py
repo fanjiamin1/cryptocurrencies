458bf7945c28f80cba89e5515179cde0c855d382
RagnarArdal <RagnarArdal@gmail.com>
2016-12-05 18:38:53 +0000

    Some test comments

--- chat/tests/test_peer.py
+++ chat/tests/test_peer.py
@@ -11,11 +11,14 @@ def test_send_receive():
     receiver_port = 8000
     receiver_address = ("127.0.0.1", receiver_port)
 
+    # Set up receiving end
     receiver = Peer(port=receiver_port)
     receiver.set_private_key(private)
 
+    # Set up sending end
     sender = Peer()
     sender.set_public_key(public)
+
     sender.send(receiver_address, message)
 
     assert receiver.receive() == message
