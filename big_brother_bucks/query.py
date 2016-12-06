afa674d8727d111686f210fbbd0e14d352e38afc
Hugo Berthier <hugo.berthier@gmail.com>
2016-12-06 11:00:11 +0000

    Added better error handling + get key thing

--- big_brother_bucks/query.py
+++ big_brother_bucks/query.py
@@ -6,31 +6,40 @@ def transaction(t_from, t_to, t_amount, t_session):
 		x.execute("INSERT INTO transaction (t_from, t_to, t_amount, t_session) VALUES (%s, %s, %s, %s)", 
 		(t_from, t_to, t_amount, t_session))
 		db.commit()
+		return x.lastrowid
 	except db.Error as e:
-		print(e)
 		db.rollback()
-	
-	return x.lastrowid
+		return e
 
 def look_up_transaction(t_id):
 	try:
 		x.execute("SELECT * FROM transaction WHERE t_id = %s", (t_id,))
 		return x.fetchall()
-	except:
+	except db.Error as e:
 		db.rollback()
+		return e
 
 def add_account():
 	try:
 		x.execute("INSERT INTO account (a_id) VALUES (null)")
 		db.commit()
-	except:
+		return x.lastrowid
+	except db.Error as e:
 		db.rollback()
-
-	return x.lastrowid
+		return e
 
 def remove_account(account_id):
 	try:
 		x.execute("DELETE FROM account WHERE a_id = %s", (account_id,))
 		db.commit()
-	except:
+		return x.lastrowid
+	except db.Error as e:
 		db.rollback()
+		return e
+
+def get_key(a_id):
+	try:
+		x.execute("SELECT a_pubkey FROM account WHERE a_id = %s", (a_id,))
+		return x.fetchall()
+	except db.Error as e:
+		return e
