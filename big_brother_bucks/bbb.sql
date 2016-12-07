cd6b453760b3f49b203146f42664ddf3ec6dd097
Hugo Berthier <hugo.berthier@gmail.com>
2016-12-07 11:25:12 +0000

    Added new accounts with their pubkeys (in that order) : hugo (8), ivar (9), ragnar (10) and the bank (11)

---
+++ big_brother_bucks/bbb.sql
@@ -0,0 +1,112 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for osx10.11 (x86_64)
+--
+-- Host: localhost    Database: cryptocurrencies
+-- ------------------------------------------------------
+-- Server version	5.6.28-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `account`
+--
+
+DROP TABLE IF EXISTS `account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `account` (
+  `a_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `a_balance` int(11) DEFAULT '0',
+  `a_pubkey` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`a_id`),
+  UNIQUE KEY `a_pubkey` (`a_pubkey`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `account`
+--
+
+LOCK TABLES `account` WRITE;
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+INSERT INTO `account` VALUES (5,9,NULL),(6,91,NULL),(7,0,'28'),(8,0,'-----BEGIN PUBLIC KEY-----MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDarjeKW1F5Xr7W+tfUukyZvTKMslOCs/kb/cFqyg0C47tl4dW0ENPEgG2JjMKwZtsLT6TdnP+flu0aIGecQyc5a5zJx60mzaDotIRLnuXvlWI9ozBAEBXVPEtR87PTeqOiRnXWFH/ha4Rza7MY6I9j5ke+Ei7x5snIporUsScTaQIDAQAB-----END PUBL'),(9,0,'-----BEGIN PUBLIC KEY-----MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDVn27M8yfJ+qegmiUGWPem5eSfzWqrTfgW+XpWuP21XBFskTHZFgHy166lI4dKhrr5g01OdLh6fl1NIQsE2Arcy3tkWqO6y4O4RynctuFgRdcy9WzKjSvad/lou68dXHxP5T5/9wfzmXc6gZWAkc+6Bn4KdRVqn8TzIB9hqnztjwIDAQAB-----END PUBL'),(10,0,'-----BEGIN PUBLIC KEY-----MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCIqUr9BzpW7tvAUjY4sizZd2MQV+Py8M/YlE+uvFgNl4yWOaWKGVrkXhsteZtqdK3/HB2SSuLLiPwtCOvvOPrJ/IQ6MSKcNfG8YPpFaN+IhpOXmvHIBDQV6U8waevggRqmoV+n1IkG1U7WTOIWwPHTWZQ1Ux2Npxe7RUW3K/FyTQIDAQAB-----END PUBL'),(11,0,'-----BEGIN PUBLIC KEY-----MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDvVAw+MKjQ+Y14qHloktE8mnKHtpARo4ovgiU3ETDn08wfkKab0lv/xd348cCuHOLcgBcDuQEd4g6Y8kpUPRq2T3nNUp7yLKaxGjUFQGWu8/jtDodD0xc95wf4/4MpTMH2AAMzWwxfjnACNGd+RCWl1E7NbxvAqZ/vBBXx/Lp5JQIDAQAB-----END PUBL');
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transaction`
+--
+
+DROP TABLE IF EXISTS `transaction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transaction` (
+  `t_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `t_from` int(11) unsigned DEFAULT '0',
+  `t_to` int(11) unsigned DEFAULT '0',
+  `t_amount` int(11) DEFAULT NULL,
+  `t_session` int(11) DEFAULT NULL,
+  PRIMARY KEY (`t_id`),
+  KEY `t_a_from` (`t_from`),
+  KEY `t_a_to` (`t_to`),
+  CONSTRAINT `t_a_from` FOREIGN KEY (`t_from`) REFERENCES `account` (`a_id`),
+  CONSTRAINT `t_a_to` FOREIGN KEY (`t_to`) REFERENCES `account` (`a_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transaction`
+--
+
+LOCK TABLES `transaction` WRITE;
+/*!40000 ALTER TABLE `transaction` DISABLE KEYS */;
+INSERT INTO `transaction` VALUES (1,5,6,100,1),(2,5,6,100,1),(3,5,6,100,1),(4,5,6,100,1),(5,6,5,200,1),(6,5,6,100,1),(7,5,6,90,1),(8,5,6,1,1);
+/*!40000 ALTER TABLE `transaction` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `update_balance` BEFORE INSERT ON `transaction` FOR EACH ROW BEGIN
+	DECLARE balance INT;
+	SELECT a_balance INTO balance FROM account WHERE a_id = NEW.t_from;
+	IF ( balance >= NEW.t_amount && NEW.t_amount > 0 ) THEN
+		UPDATE 
+			account a1 JOIN account a2 
+			ON a1.a_id = NEW.t_from AND a2.a_id = NEW.t_to
+			SET
+				a1.a_balance = a1.a_balance - NEW.t_amount,
+				a2.a_balance = a2.a_balance + NEW.t_amount;
+	ELSE
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "Unsufficient amount, transaction aborted";  
+	END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-12-07 11:23:11
