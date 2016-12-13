0d53975caafebc3313fd4091787f3a9776a0753e
RagnarArdal <RagnarArdal@gmail.com>
2016-12-13 16:02:31 +0000

    Thrall is cool

--- blockchain_agreement/simulation.py
+++ blockchain_agreement/simulation.py
@@ -17,11 +17,14 @@ from this import s as rot_13_this
 sys.stdout = actual_stdout
 
 
+DOCSIZE = Block.PAYLOAD_SIZE - 32 - 32 - 1 - 16
+
+
 # Build genesis block
 # 32 bytes of padding+timestamp
 genesis_payload = time.ctime(1000198000).zfill(Block.HASH_POINTER_SIZE).encode()
 # Block.PAYLOAD_SIZE-32-32-1-16 bytes of documents+padding
-genesis_payload += rot_13_this.encode().ljust(Block.PAYLOAD_SIZE - 32 - 32 - 1 - 16)
+genesis_payload += rot_13_this.encode().ljust(DOCSIZE)
 # 32 byte counter starting from zero
 genesis_payload += bytes(32)
 # 1 byte representing difficulty of work
@@ -43,38 +46,7 @@ blockchain = Blockchain(genesis_block)
 communication_lock = threading.Lock()
 sockets = []
 miner_ids = []
-
-
-class MinerDelegate(threading.Thread):
-    def __init__(self,block_contents,target):
-        #accepts blocks in the form of a list of strings
-        #each of which is a field of the previous block
-        super(StoppableThread,self).__init__()
-        self.stopped=False
-        self.prehash=SHA()
-
-        self.nonce=0
-        for item in block_contents:
-            self.prehash.update(str.encode(item))
-
-        self.found=False
-
-    def stop(self):
-        self.stopped=True
-
-    def myhash(self):
-        #steal this from proofofwork
-        temphash=self.prehash.copy()
-        temphash.update(str.encode(str(self.nonce)))
-        self.nonce+=1
-        return temphash.digest()
-
-    def run(self):
-        while not stopped:
-            guess=hash()
-            if myHash[0:len(target)] == target:
-                self.found=True
-                return
+miners = []
 
 
 def broadcast(byte_array):
@@ -84,9 +56,83 @@ def broadcast(byte_array):
 
 def read_socket(miner_id):
     try:
-        return sockets[miner_id].popleft()
+        with communication_lock:
+            return sockets[miner_id].popleft()
     except IndexError:
         return None
+
+
+class Thrall(threading.Thread):
+    def __init__(self, latest_block, payload):
+        # Prehash
+        hash_ptr = Hash()
+        hash_ptr.update(latest_block.hash_pointer)
+        hash_ptr.update(latest_block.payload)
+        self.prehash = Hash()
+        self.prehash.update(hash_ptr.digest())
+        self.prehash.update(time.ctime().zfill(Block.HASH_POINTER_SIZE).encode())
+        self.prehash.update(payload.zfill(DOCSIZE))
+        old_counter = latest_block.payload[-17-32:-17]
+        assert len(old_counter) == 32
+        self.prehash.update(old_counter)  # TODO
+        difficulty = latest_block.payload[-16-1:-16]
+        assert len(difficulty) == 1
+        assert difficulty[0] == 24
+        self.prehash.update(difficulty)
+
+        # Other attributes
+        self.stopped = False
+        self.found = False
+
+        super(Thrall, self).__init__()
+
+    def stop(self):
+        self.stopped = True
+
+    def myhash(self):
+        return 1
+        #temphash = self.prehash.copy()
+        #temphash.update(str.encode(str(self.nonce)))
+        #self.nonce+=1
+        #return temphash.digest()
+
+    def run(self):
+        while not self.stopped:
+            pass
+            #guess = hash()
+            #if myHash[0:len(target)] == target:
+            #    self.found=True
+            #    return
+
+
+class Miner(threading.Thread):
+    def __init__(self, id_number):
+        self.id_number = id_number
+        super(Miner, self).__init__()
+
+    def run(self):
+        while True:
+            message = read_socket(self.id_number)
+            if message is None:
+                continue
+            else:
+                blockchain = pickle.loads(message)
+                if not isinstance(blockchain, Blockchain):
+                    print("Surprising junk on mah socket, bby")
+                    continue
+                self.blockchain = blockchain
+                break
+        print(self.blockchain)
+        payload = b"I, miner number "
+        payload += bytes([ord(str(self.id_number))])
+        payload += b" did this!"
+        print(payload)
+        self.slave = Thrall(blockchain.latest_block, payload)
+        self.slave.run()
+        while self.slave.found is False:
+            pass
+        print(self.slave.result)
+        self.slave.join()
 
 
 if __name__ == "__main__":
@@ -94,26 +140,25 @@ if __name__ == "__main__":
     if len(args) != 1:
         sys.exit()
     try:
-        miners = int(args[0])
+        total_miners = int(args[0])
     except:
         sys.exit()
 
-    for number in range(miners):
+    # Create miners
+    for miner_id in range(total_miners):
+        print("Creating miner", miner_id)
         sockets.append(collections.deque())
-        print("Creating miner", number)
-        miner_ids.append(number)
+        miner_ids.append(miner_id)
+        miners.append(Miner(miner_id))
 
+    # Broadcast block
     print("Broadcasting genesis block in a single block blockchain")
     broadcast(pickle.dumps(blockchain))
 
+    # Start miners
     for miner_id in miner_ids:
         print("Starting miner", miner_id)
+        miners[miner_id].start()
 
-    for miner_id in miner_ids:
-        while True:
-            message = read_socket(miner_id)
-            if message is None:
-                print("Miner", miner_id, "received nothing")
-                break
-            else:
-                print("Miner", miner_id, "received something!")
+    for miner in miners:
+        miner.join()
