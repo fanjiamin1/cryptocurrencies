6f64a57318e566f85d072d80279c948a9a2fd5fd
Ívar Örn Ragnarsson <ivarr15@ru.is>
2016-12-16 22:09:03 +0000

    division modulo p fixed

--- secrets/secretsharing.py
+++ secrets/secretsharing.py
@@ -1,14 +1,17 @@
 from random import randint
+from functools import reduce
 
 
 
 def get_shares(k,n,S):
 
-
     #TODO: make big prime P as a basis for working in Zp
-    topprime=5
+    topprime=10009
     
-    coefficients=[randint(1,topprime) for x in range(k)]
+    #want to create a polynomial of degree k-1 so we need k constants
+    #we dissallow coefficients of 0 for simplicity
+    coefficients=[S]
+    coefficients.extend(randint(1,topprime-1) for _ in range(k-1))
 
     used_xvals=[]
     found_yvals=[]
@@ -16,19 +19,44 @@ def get_shares(k,n,S):
     for i in range(n):
         xval=randint(1,topprime-1)
         while xval in used_xvals:
+            #can be infinite, maybe do something about that,
+            #call needs to be malfromed for that to happen though
             xval=randint(1,topprime-1)
-        used_xvals.append(x)
-        found_yvals.append(evaluate_polynomial(x,coefficients)%topprime)
+        used_xvals.append(xval)
+        found_yvals.append(evaluate_polynomial(xval,coefficients)%topprime)
     shares=[(used_xvals[i],found_yvals[i]) for i in range(n)]
-    return p,shares
+    return topprime,shares
 
 
 
 def evaluate_polynomial(x,coefficients):
     output=0
-    for i in range(len(coefficients):
-        sum+=coefficients[i]*x**i
+    for i in range(len(coefficients)):
+        output+=coefficients[i]*x**i
     return output
+
+
+def divmod(a,b,p):
+    #Division in the module P
+    #adapted from wikipedia psuedocode
+    s=0
+    s_prev=1
+    t=1
+    t_prev=0
+    r=b
+    r_prev=p
+    while r!=0:
+        quotient=r_prev//r
+        r_prev,r=r,r_prev - quotient*r
+        s_prev,s=s,s_prev - quotient*s
+        t_prev,t=t,t_prev - quotient*t
+    #either s_prev or t_prev is the inverse of b
+    #return a*s_prev or return a*t_prev
+    return a*t_prev
+
+
+    
+
 
 
 def combine(k,n,p,shares):
@@ -39,18 +67,35 @@ def combine(k,n,p,shares):
     xvals=[x[0] for x in shares]
     yvals=[x[1] for x in shares]
     secretsum=0
-    sum([ 
-    denominators=[yvals[j]*reduce(
-                operator.mul,[xvals[i] for i in range(k) where i!=j])
+    denominators=[yvals[j]*reduce
+                 (
+                    lambda x,y: x*y,
+                    [xvals[i] for i in range(k) if i!=j]
+                 )
                 for j in range(k)
                 ]
-    divisors = [ reduce(operator.mul,
-                [xvals[i]-xvals[j] for i in range(k) where i!=j]
-                for j in range[k]]
-    secretsum=sum([denominators[i]/divisors[i] for i in range(k)]
+    divisors = [ reduce(
+                        lambda x,y: x*y,
+                        [xvals[i]-xvals[j] for i in range(k) if i!=j]
+                       )
+                for j in range(k)
+                ]
+    secretsum=sum([divmod(denominators[i],divisors[i],p) for i in range(k)])
 
-    return secretsum
+    return secretsum%p
         
+testsecret=9001
 
+print("testing divmod")
+print("3/4 * 4 in the module p should give 3")
+testprime=17
+print(4*divmod(3,4,testprime) % testprime)
+p,shares=get_shares(3,5,testsecret)
+for share in shares:
+    print(share)
+
+combinedsecret=combine(3,5,p,shares[:3])
+print('original secret was:',testsecret)
+print('extracted secret was:',combinedsecret)
 
     
